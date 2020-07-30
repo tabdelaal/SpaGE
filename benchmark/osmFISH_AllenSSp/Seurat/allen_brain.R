a89ef76bc0011aadf33c116d6fe293caa3070e3a
tabdelaal <31687847+tabdelaal@users.noreply.github.com>
2020-07-30 14:31:19 +0200

    FilterLQ_cells

--- benchmark/osmFISH_AllenSSp/Seurat/allen_brain.R
+++ benchmark/osmFISH_AllenSSp/Seurat/allen_brain.R
@@ -5,7 +5,12 @@ allen <- read.table(file = "data/Allen_SSp/SSp_exons_matrix.csv",
                     row.names = 1,sep = ',', stringsAsFactors = FALSE, header = TRUE)
 allen <- as.matrix(x = allen)
 
+meta.data <- read.csv(file = "data/Allen_SSp/AllenSSp_metadata.csv",
+                      row.names = 1, stringsAsFactors = FALSE)
+
 al <- CreateSeuratObject(counts = allen, project = 'SSp', min.cells = 10)
+ok.cells <- colnames(al[,meta.data$class_label != 'Exclude'])
+al <- al[, ok.cells]
 al <- NormalizeData(object = al)
 al <- FindVariableFeatures(object = al, nfeatures = 2000)
 al <- ScaleData(object = al)
