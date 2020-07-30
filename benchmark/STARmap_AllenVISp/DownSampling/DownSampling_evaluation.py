e2d4b9e86b8dff0f42d87ae1f4628fd87da02ba8
tabdelaal <31687847+tabdelaal@users.noreply.github.com>
2020-07-30 14:34:34 +0200

    Add_step_500genes

--- benchmark/STARmap_AllenVISp/DownSampling/DownSampling_evaluation.py
+++ benchmark/STARmap_AllenVISp/DownSampling/DownSampling_evaluation.py
@@ -55,6 +55,13 @@ SpaGE_imputed_200 = pd.read_csv('Results/200SpaGE_New_genes.csv',header=0,index_
 SpaGE_Corr_200 = pd.Series(index = test_set)
 for i in test_set:
     SpaGE_Corr_200[i] = st.spearmanr(Starmap_data[i],SpaGE_imputed_200[i])[0]
+    
+#500    
+SpaGE_imputed_500 = pd.read_csv('Results/500SpaGE_New_genes.csv',header=0,index_col=0,sep=',')
+
+SpaGE_Corr_500 = pd.Series(index = test_set)
+for i in test_set:
+    SpaGE_Corr_500[i] = st.spearmanr(Starmap_data[i],SpaGE_imputed_500[i])[0]
 
 #944    
 SpaGE_imputed_944 = pd.read_csv('Results/944SpaGE_New_genes.csv',header=0,index_col=0,sep=',')
@@ -71,25 +78,28 @@ plt.boxplot([SpaGE_Corr_10, SpaGE_Corr_30, SpaGE_Corr_50,
 
 y = SpaGE_Corr_10
 x = np.random.normal(1, 0.05, len(y))
-plt.plot(x, y, 'r.', alpha=0.2)
+plt.plot(x, y, 'g.', alpha=0.2)
 y = SpaGE_Corr_30
 x = np.random.normal(2, 0.05, len(y))
-plt.plot(x, y, 'r.', alpha=0.2)
+plt.plot(x, y, 'g.', alpha=0.2)
 y = SpaGE_Corr_50
 x = np.random.normal(3, 0.05, len(y))
-plt.plot(x, y, 'r.', alpha=0.2)
+plt.plot(x, y, 'g.', alpha=0.2)
 y = SpaGE_Corr_100
 x = np.random.normal(4, 0.05, len(y))
-plt.plot(x, y, 'r.', alpha=0.2)
+plt.plot(x, y, 'g.', alpha=0.2)
 y = SpaGE_Corr_200
 x = np.random.normal(5, 0.05, len(y))
-plt.plot(x, y, 'r.', alpha=0.2)
-y = SpaGE_Corr_944
+plt.plot(x, y, 'g.', alpha=0.2)
+y = SpaGE_Corr_500
 x = np.random.normal(6, 0.05, len(y))
-plt.plot(x, y, 'r.', alpha=0.2)
+plt.plot(x, y, 'g.', alpha=0.2)
+y = SpaGE_Corr_944
+x = np.random.normal(7, 0.05, len(y))
+plt.plot(x, y, 'g.', alpha=0.2)
 
 
-plt.xticks((1,2,3,4,5,6),('10','30', '50','100','200','944'),size=10)
+plt.xticks((1,2,3,4,5,6,7),('10','30', '50','100','200','500','944'),size=10)
 plt.yticks(size=8)
 plt.xlabel('Number of shared genes',size=12)
 plt.gca().set_ylim([-0.3,0.8])
